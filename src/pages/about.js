055973584d185d4b2d0b4d880607d79748cfd454
salimsdev <salim_med@msn.com>
2021-10-14 02:03:56 +0200

    contact and about pages done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,128 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Layout from '../components/Layout';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Seo from '../components/Seo';
 
 const useStyles = makeStyles(theme => ({
-
+    container: {
+        margin: '3rem auto',
+        padding: '2rem 4rem',
+        [theme.breakpoints.down('sm')]: {
+            margin: '1rem auto',
+            padding: '0 0.3rem'
+        }
+    },
+    title: {
+        fontFamily: 'Dancing Script',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '3rem 0 4rem'
+    },
+    text: {
+        fontFamily: 'Dancing Script',
+        fontSize: '1.6rem',
+        fontWeight: 500,
+        marginBottom: '1rem'
+    },
+    team: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center'
+    },
+    subtitle: {
+        fontFamily: 'Dancing Script',
+        fontWeight: 500,
+        margin: '2rem 0 1.5rem',
+        [theme.breakpoints.down('sm')]: {
+            margin: '0.5rem 0 1rem',
+        }
+    },
+    author: {
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '1rem'
+    },
+    avatar: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+        marginRight: '1rem'
+    }
 }));
 
-const About = () => {
+const About = ({ data }) => {
     const classes = useStyles();
+    const authors = data.authors.nodes;
 
     return (
         <Layout>
-            <Container>
-                ABOUT
+            <Seo title='A propos' isBlogPost={false} />
+            <Container maxWidth='lg' className={classes.container}>
+                <Typography variant='h2' color='textPrimary' className={classes.title} align='center'>
+                    Nous sommes avec vous!
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text}>
+                    J'ai créé ce site avec l'objectif d'inspirer les lecteurs, de les motiver et de les aider à évoluer pour devenir la meilleure version d'eux-même. 
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text}>
+                    Mon souhait est que nous devenions une référence francophone dans le domaine de la santé et du bien-être, en fournissant un contenu de qualité et 
+                    des conseils de haut niveau pour les passionnés de fitness, de beauté, de nutrition à la recherche d'un meilleur style de vie.
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text}>
+                    Nous donnons le maximum pour être une source de motivation en offrant un contenu qui vous aidera à réussir!
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text}>
+                    Nos lecteurs reviennent régulièrement découvrir nos derniers articles sur les nouvelles tendances bien-être et santé. Notre contenu est exclusivement 
+                    écrit pour vous par nos experts dans les domaines de la santé, du fitness, de la nutrition et de la psychologie. Notre mission commune est de vous donner
+                    les outils nécessaires pour vivre plus heureux.
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text}>
+                    Vous n'êtes pas seul. Nous sommes avec vous!
+                </Typography>
+                <Typography variant='body1' color='textPrimary' className={classes.text} align='right'>
+                    Alice.
+                </Typography>
+                <Grid container spacing={2}>
+                    <Grid item sm={5} className={classes.team}>
+                        <Typography variant='h4' color='textPrimary' className={classes.subtitle}> Notre équipe:</Typography>
+                        {authors.map(({ name, photo, id}) => {
+                            const avatarImage = getImage(photo);
+                            return (
+                                <div className={classes.author} key={id}>
+                                    <Avatar className={classes.avatar}>
+                                        <GatsbyImage image={avatarImage} alt={name} />
+                                    </Avatar>
+                                    <Typography>{name}</Typography>
+                                </div>
+                            )
+                        })}
+                    </Grid>
+                    <Grid item sm={7}>
+                        <StaticImage src='../assets/images/about.jpg' alt='chemin de fleurs' placeholder='blurred' layout='constrained' />
+                    </Grid>
+                </Grid>
             </Container>
         </Layout>
     );
 };
+
+export const query = graphql`
+    {
+        authors: allContentfulAuthors (sort: {fields: name, order: ASC}) {
+            nodes {
+                name
+                photo {
+                gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+                }
+                id
+            }
+        }
+    }
+`
 
 export default About;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,35 @@ import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import Layout from '../components/Layout';
+import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const useStyles = makeStyles(theme => ({
-
+    container: {
+        margin:'0 auto 4rem',
+        padding: '0 4rem'
+    },
+    title: {
+        fontFamily: 'Dancing Script',
+        marginBottom: '2rem'
+    },
+    img: {
+        height: '65vh',
+        marginBottom: '4rem'
+    },
+    text: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    btn: {
+        marginTop: '1rem'
+    }
 }));
 
 const Contact = () => {
@@ -13,8 +38,26 @@ const Contact = () => {
 
     return (
         <Layout>
-            <Container>
-                Contact
+            <Seo title='Contact' isBlogPost={false} />
+            <Container maxWidth='lg' className={classes.container}>
+                <StaticImage src='../assets/images/contact.jpg' alt='hello' placeholder='blurred' layout='constrained' className={classes.img} />
+                <Grid container spacing={6}>
+                    <Grid item sm={5} className={classes.text}>
+                        <Typography variant='h3' color='textPrimary' className={classes.title} align='center'>Nous contacter</Typography>
+                        <Typography variant='body1' color='textPrimary'>
+                            Vous avez des questions, des requêtes, un problème technique ou vous souhaitez simplement nous envoyer un message? N'hésitez pas à nous en faire part en utilisant le formulaire.
+                        </Typography>
+                    </Grid>
+                    <Grid item sm={7}>
+                        <form name='contact' method='POST' data-netlify='true'>
+                            <input type='hidden' name='form-name' value='contact' />
+                            <TextField variant='outlined' label="Prénom" type="text" name='name' fullWidth autoFocus={true} margin='normal' placeholder='Entrez votre prénom' required />
+                            <TextField variant='outlined' label="Email" type="email" name='email' fullWidth margin='normal' placeholder='Entrez votre email' required />
+                            <TextField variant='outlined' label="Message" name="message" fullWidth multiline={true} margin='normal' placeholder='Entrez votre message' required minRows={3} />
+                            <Button type='submit' fullWidth variant='outlined' color='primary' className={classes.btn}>envoyer</Button>
+                        </form>
+                    </Grid>
+                </Grid>
             </Container>
         </Layout>
     );
