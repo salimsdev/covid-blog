224bcde4027529fe5958077d1e5a1c30af24407d
salimsdev <salim_med@msn.com>
2023-09-21 02:06:43 +0200

    propellerAds reactivation

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,11 @@
 const React = require("react")
 
-exports.onRenderBody = ({setHeadComponents}) => {
-    // setPostBodyComponents([
-    //     <script key="1" type="text/javascript" src={"propellerAdsScriptInPagePush.js"}/>,
-    //     <script key="2" type="text/javascript" src={"propellerAdsScriptVignette.js"}/>
-    // ]);
-    setHeadComponents([
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6881284677253875" crossorigin="anonymous"></script>
-    ])
+exports.onRenderBody = ({setPostBodyComponents}) => {
+    setPostBodyComponents([
+        <script key="1" type="text/javascript" src={"propellerAdsScriptInPagePush.js"}/>,
+        <script key="2" type="text/javascript" src={"propellerAdsScriptVignette.js"}/>
+    ]);
+    // setHeadComponents([
+    //     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6881284677253875" crossorigin="anonymous"></script>
+    // ])
 };
